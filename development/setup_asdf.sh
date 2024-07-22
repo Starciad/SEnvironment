9f9b84a18d651211e6c775e59022a01f402ad71c
Starciad <davigamerlsf16@gmail.com>
2024-07-22 20:54:40 -0300

    feat: add new installation packages.

---
+++ development/setup_asdf.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Configurando o ASDF"
+
+# Plugins
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin add python https://github.com/asdf-community/asdf-python
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
+asdf plugin-add php https://github.com/asdf-community/asdf-php.git
+asdf plugin-add rust https://github.com/asdf-community/asdf-rust.git
+asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
+asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+asdf plugin add kotlin https://github.com/asdf-community/asdf-kotlin.git
+asdf plugin-add crystal https://github.com/asdf-community/asdf-crystal.git
+asdf plugin-add lua https://github.com/Stratus3D/asdf-lua.git
+asdf plugin add dotnet https://github.com/hensou/asdf-dotnet.git
+asdf plugin-add java https://github.com/halcyon/asdf-java.git
+asdf plugin-list
+
+# Install
+asdf install nodejs latest
+
+# Globals
+asdf global nodejs latest
+
+echo "ASDF configurado!"
